5e3bfe1dd3856e4bed7da03b4472ac3e8b6218a2
agnieszka <agaxx02@gmail.com>
2022-07-09 15:31:16 +0200

    Registration works

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ const PORT = 8000;
 
 require('dotenv').config({path: './config/.env'})
 
+const ObjectId = require('mongoose').Types.ObjectId
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String, 
+  confirmPassword: String,
+})
+
+const User = mongoose.model('User', userSchema)
+
 const connectDB = async () => {
     try {
       const conn = await mongoose.connect(process.env.DB_STRING, {
@@ -37,7 +47,29 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
-app.post('/register', (req,res) => {
+app.post('/register', async (req,res) => {
     const { username, password, confirmPassword } = req.body
-    res.send({username, password, confirmPassword})
+    const user = await User.findOne({ username }).exec();
+    if(user){
+      res.status(500);
+      res.json({
+        message: 'User already exists'
+      });
+      return;
+    }else if(password.length<8 || password.toLowerCase()===password){
+      res.status(500);
+      res.json({
+        message: 'Password has to be at least 8 characters long and contain at least one uppercase letter'
+      });
+      return
+    }else if(password !== confirmPassword){
+      res.status(500);
+      res.json({
+        message: 'Passwords are different'
+      });
+      return
+    }await User.create({ username, password });
+    res.json({
+      message: 'User successfully created'
+    })
 });
