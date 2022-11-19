b197d6f13cfa3b1b53c5605ba0f4440ecdc3ce1b
agnieszka <agaxx02@gmail.com>
2022-11-19 16:19:36 +0100

    Changed User profile

--- src/models/User.js
+++ src/models/User.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
 	username: String,
 	password: String,
+	picture: String,
+	goal: Number,
 });
 
 module.exports = mongoose.model('User', userSchema);
